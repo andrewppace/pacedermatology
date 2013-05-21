2b8ae614e7b465806d4aa790a9edb1fb835679c3
Andrew Pace <andrewppace@gmail.com>
2013-05-20 19:50:38 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pacedermatology::Application.config.secret_token = '78899b78e772081f925f5b6b28c4cfa2d86d0e56e0388d5d1d1900b93bb16a0bdf7e904f770b2e6ca5b2ba288c47ae8ba44c3bde9c6799c38facb7de5144c244'
